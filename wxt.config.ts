bb07b2cf038892f7a783e450f8fd599d1c576d45
Yasuhiro ARAKI <iron@araki.net>
2026-06-20 06:25:08 +0900

    chore(release): bump version to 6.0.2

--- wxt.config.ts
+++ wxt.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     manifest_version: 3,
     name: '__MSG_extensionName__',
     short_name: '__MSG_extensionShortName__',
-    version: '6.0.1',
+    version: '6.0.2',
     description: '__MSG_extensionDescription__',
     default_locale: 'en',
     homepage_url: 'https://github.com/armaniacs/yasumaro',
